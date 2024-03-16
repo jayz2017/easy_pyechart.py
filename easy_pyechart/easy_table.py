b8d34205cc922ec3bb539e9e60d2eb64c93e1b41
chenhao <1012478704@qq.com>
2024-03-16 16:21:08 +0800

    修改

--- easy_pyechart/easy_table.py
+++ easy_pyechart/easy_table.py
@@ -39,13 +39,28 @@ class eMTable():
                            'outSaveUrl':outSaveUrl
                            }
     def base_table(self):
-        fig, ax = plt.subplots()    
+        # 设置图像大小  
+        figsize = (8, 6)  
+        fig, ax = plt.subplots(figsize=figsize)
         # 隐藏坐标轴
         ax.axis("off")
-        table = ax.table(cellText=self.opts['rowList'], cellColours=self.opts['columnColor'], loc="center")
-        table.set_edges("closed")
-        plt.savefig(self.opts['outSaveUrl'])
+        table = ax.table(cellText=self.opts['rowList'], 
+                         #cellColours=self.opts['columnColor'], 
+                         cellLoc='center',
+                         loc="center")
+      
+        colors = ['lightblue', 'pink', 'lightgreen', 'yellow', 'lightcoral']
+        # 设置列宽和行高  
+        for (i, j), cell in table.get_celld().items():  
+            cell.set_width(0.25)  
+            cell.set_height(0.08)
+            cell.set_facecolor('lightblue')  
+                # 设置表格属性（可选） 
+        table.auto_set_font_size(False)  
+        table.set_fontsize(18)  
+        table.scale(1, 1.5)  # 调整表格大小  
+        plt.savefig(self.opts['outSaveUrl'],bbox_inches='tight', pad_inches=0)
 
 
 
-#print(sys.path)
+eMTable(rowList=[[1, 2, 3,3,4,4,4,4,4], [4, 5, 6,3,4,4,4,4,4], [7, 8, 9,3,4,4,4,4,4], [7, 8, 9,3,4,4,4,4,4], [7, 8, 9,3,4,4,4,4,4], [7, 8, 9,3,4,4,4,4,4], [7, 8, 9,3,4,4,4,4,4]],outSaveUrl='www.png').base_table()
